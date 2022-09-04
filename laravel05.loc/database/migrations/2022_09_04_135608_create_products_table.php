3e63aac180307780453cefe586070793cb6dd1c9
maxpol1 <maxolhevski@mail.ru>
2022-09-04 22:04:08 +0300

    add_product_table_loc

--- laravel05.loc/database/migrations/2022_09_04_135608_create_products_table.php
+++ laravel05.loc/database/migrations/2022_09_04_135608_create_products_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', '255');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->binary('image');
+            $table->double('price');
+            $table->integer('active');
         });
     }
 
