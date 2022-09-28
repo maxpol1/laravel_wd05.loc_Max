6eab9013c0fe926cc3239ba4edcdfbb8b1976346
maxpol1 <maxolhevski@mail.ru>
2022-09-28 11:09:33 +0300

    refactor

---
+++ laravel05.loc/laravel05.loc/app/Http/Controllers/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use function Sodium\add;
+
+class SiteController extends Controller
+{
+    public function __invoke()
+    {
+//        select * from products where active = 1
+//        and category_id = 1
+//        limit 10
+//        order by id desc
+
+        $latestProducts = Product::query()
+            //->select(['id', 'name', 'image'])
+            //->orderBy('id', 'desc') или
+            ->limit(10)
+            ->where('active', 1)
+            ->latest()
+            ->get();
+
+        return view('site.index', compact('latestProducts'));
+    }
+}
